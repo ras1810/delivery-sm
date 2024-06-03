3c3c25d166bc2c932e0e68aeb8983e84e897518a
Rodrigo Soria <rodrigoalejandrosoria1@gmail.com>
2024-06-03 17:22:39 -0300

    03/06/24 cambios - mejoras

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8080 || undefined;
 
 app.use(express.static('public'));
-
+//! instalar middleware cors
 /** GET */
 app.get('/', (req, res) => {
     res.render('index.html');
